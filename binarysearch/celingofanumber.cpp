0c593cfc27ad6eb382f7187080f1e8550ba49a18
trilok singh rana <triloksinghrana309@gmail.com>
2025-03-23 12:02:04 +0530

    program

---
+++ binarysearch/celingofanumber.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int arr[7]={2,3,5,9,14,16,18};
+  int start=0;
+  int end=sizeof(arr)-1;
+  int target=15;
+  while(start<=end){
+    int mid=start+(end-start)/2;
+    if(arr[mid]==target){
+       return mid;
+    }
+    else if(arr[mid]<=target){
+      start=mid+1;
+    }
+    else{
+      end=mid-1;
+    }
+  }
+  cout<< start;
+}
