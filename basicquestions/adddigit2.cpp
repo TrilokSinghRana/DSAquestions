7b21a5c0da0e9b84e0fada6ccee3bba57f3cb075
trilok singh rana <triloksinghrana309@gmail.com>
2025-07-05 18:29:23 +0530

    my code

--- basicquestions/adddigit2.cpp
+++ basicquestions/adddigit2.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 int main()
 {
-  int num=199;
-  int ans=0;
+
   while(num>9){
-    while(num!=0){
-       int rem=num%10;
-        ans=ans+rem;
-       num=num/10;
-    }
-  }
-   num=ans;
-  cout<< num;
+        int ans=0;
+
+        while(num!=0){
+          int rem=num%10;
+          ans=ans+rem;
+          num=num/10;
+        }
+        num=ans;
+       } 
+       return num;
 }
