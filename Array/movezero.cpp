5629570e28cfbc549cda4ef7ce3a6521ab48e98a
trilok singh rana <triloksinghrana309@gmail.com>
2025-09-02 21:32:37 +0530

    my code

--- Array/movezero.cpp
+++ Array/movezero.cpp
@@ -3,20 +3,18 @@ using namespace std;
 int main()
 {
   int arr[6]={0,1,0,3,12,0};
-  int n=sizeof(arr)-1;
+  int n=6;
   int start=0;
-  int end=n;
-  while(start<=end){
-    if(arr[end]==0){
-      end--;
-    }
-    else if(arr[start]>=0){
-      swap(arr[start],arr[end]);
-      start++;
-      end--;
-    }
-    else{
-
+  int end=n-1;
+  int i=0;
+  for(int j=0;j<n;j++){
+    if(arr[j]!=0){
+       swap(arr[j],arr[i]);
+       i++;
     }
   }
+  for(int j=0;j<n;j++){
+    cout<<arr[j];
+  }
+
 }
