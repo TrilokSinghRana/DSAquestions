94449af03bcb4a2fc62246d4b4414c57ff30e2d1
trilok singh rana <triloksinghrana309@gmail.com>
2025-09-27 20:59:35 +0530

    my code

--- binarysearch/peakindexinmountainarray.cpp
+++ binarysearch/peakindexinmountainarray.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 int main()
 {
-  
+  int arr[7]={2,4,6,8,10,8,5};
+  int start=0;
+  int end=6;
+  while(start<=end){
+    int mid=start+(end-start)/2;
+    if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+     cout<<mid;
+     return 0;
+    }
+    else if(arr[mid]>arr[mid+1]){
+    end=mid-1;
+    }
+    else{
+      start=mid+1;
+    }
+  }
+  return -1;
 }
